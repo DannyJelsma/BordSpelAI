a4900e77e30637018c98d4ede0cbf84923d1db64
Danny Jelsma <danny.jelsma@hotmail.com>
2021-03-31 17:09:10 +0200

    Error fix

--- src/main/java/nl/hanze/bordspelai/listeners/MatchStartListener.java
+++ src/main/java/nl/hanze/bordspelai/listeners/MatchStartListener.java
@@ -1,6 +1,8 @@
 package nl.hanze.bordspelai.listeners;
 
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import nl.hanze.bordspelai.BordspelAI;
 import nl.hanze.bordspelai.controllers.GameController;
 import nl.hanze.bordspelai.events.NetEventListener;
 import nl.hanze.bordspelai.managers.SceneManager;
@@ -10,6 +12,7 @@ import nl.hanze.bordspelai.notifications.Notification;
 import java.util.Map;
 
 public class MatchStartListener implements NetEventListener {
+
     @Override
     public void update(Notification notification) {
         if (notification.getNotificationType().equals("MATCH")) {
@@ -17,15 +20,17 @@ public class MatchStartListener implements NetEventListener {
             String gameType = dataMap.get("GAMETYPE");
 
             if (gameType.equalsIgnoreCase("Tic-tac-toe")) {
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                String opponent = dataMap.get("OPPONENT");
+                BordspelAI.getThreadPool().submit(() -> Platform.runLater(() -> {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    String opponent = dataMap.get("OPPONENT");
 
-                alert.setTitle("Match started");
-                alert.setHeaderText(null);
-                alert.setContentText("A match of " + gameType + " has started. Playing against: " + opponent);
-                alert.show();
+                    alert.setTitle("Match started");
+                    alert.setHeaderText(null);
+                    alert.setContentText("A match of " + gameType + " has started. Playing against: " + opponent);
+                    alert.show();
 
-                SceneManager.switchScene("/views/game.fxml", new GameController(new TicTacToeModel()));
+                    SceneManager.switchScene("/views/game.fxml", new GameController(new TicTacToeModel()));
+                }));
             }
         }
     }
